be9a5abd561a1c2ca365088997da1753310d903c
Roel Adriaans <roel@adriaans.org>
2021-12-05 23:20:20 +0100

    Implement day 5, part 2

--- src/adventofcode2021/solutions/day05.py
+++ src/adventofcode2021/solutions/day05.py
@@ -1,8 +1,13 @@
+import logging
 import re
+import typing
 from collections import Counter
 from dataclasses import dataclass
+from typing import Tuple
 
 from adventofcode2021.utils.abstract import FileReaderSolution
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -12,7 +17,7 @@ class Point:
 
 
 class Day05:
-    def parse_lines(self, line) -> (Point, Point):
+    def parse_lines(self, line) -> Tuple[Point, Point]:
         """ "
         Parse 2,2 -> 2,1 or 419,207 -> 419,109
         As x1,y1 -> x2,y2
@@ -24,19 +29,52 @@ class Day05:
         p2 = Point(x=int(matches["x2"]), y=int(matches["y2"]))
         return p1, p2
 
-    def find_overlap(self, points: list[(Point, Point)]) -> list[Point]:
+    def find_overlap(self, points: list[Tuple[Point, Point]]) -> list[Point]:
         """Find the overlapping locations in the list of points"""
-        locations = Counter()
+        locations: typing.Counter = Counter()
 
         for line in points:
-            # Make sure that the start is lower then the end. range(5, 0) does not work
-            x_start = min(line[0].x, line[1].x)
-            x_stop = max(line[0].x, line[1].x)
-            y_start = min(line[0].y, line[1].y)
-            y_stop = max(line[0].y, line[1].y)
-            for x in range(x_start, x_stop + 1):
-                for y in range(y_start, y_stop + 1):
-                    locations[(x, y)] += 1
+            # Make sure that the start is lower than the end. range(5, 0) does not work
+
+            if line[0].x == line[1].x or line[0].y == line[1].y:
+                x_start = min(line[0].x, line[1].x)
+                x_stop = max(line[0].x, line[1].x)
+                y_start = min(line[0].y, line[1].y)
+                y_stop = max(line[0].y, line[1].y)
+
+                # Horizontal or vertical line
+                for x in range(x_start, x_stop + 1):
+                    for y in range(y_start, y_stop + 1):
+                        locations[(x, y)] += 1
+            else:
+                # Vertical line. Find out the length of the vector:
+                # Always 45 degree lines
+                x1 = line[0].x
+                x2 = line[1].x
+                y1 = line[0].y
+                y2 = line[1].y
+
+                if x1 < x2:
+                    length = x2 - x1
+                    x_direction = 1
+                else:
+                    length = x1 - x2
+                    x_direction = -1
+                if y1 < y2:
+                    y_direction = 1
+                else:
+                    y_direction = -1
+
+                for i in range(length + 1):
+                    debug_txt = (
+                        f"For line ({line[0].x}, {line[0].y}) -> "
+                        f"({line[1].x}, {line[1].y}) adding point: "
+                        f"({(x1 + (i * x_direction))}, {(y1 + (i * y_direction))}) : "
+                        f"{i} of {length}"
+                    )
+                    logger.debug(debug_txt)
+
+                    locations[(x1 + (i * x_direction)), (y1 + (i * y_direction))] += 1
 
         # Filter out locations with at least 2 hits
         return [location[0] for location in locations.items() if location[1] > 1]
@@ -56,4 +94,10 @@ class Day05PartA(Day05, FileReaderSolution):
 
 class Day05PartB(Day05, FileReaderSolution):
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        points = []
+        for line in input_data.splitlines():
+            p1, p2 = self.parse_lines(line)
+            points.append((p1, p2))
+
+        res = self.find_overlap(points)
+        return len(res)
