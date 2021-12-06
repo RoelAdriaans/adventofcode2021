03e148c2326b23a58457f02c40b4d5dabe0f3d01
Roel Adriaans <roel@adriaans.org>
2021-12-06 22:07:26 +0100

    Do not create a new Counter, and remove the useless day integer

--- src/adventofcode2021/solutions/day06.py
+++ src/adventofcode2021/solutions/day06.py
@@ -5,29 +5,24 @@ from adventofcode2021.utils.abstract import FileReaderSolution
 
 class Day06:
     fish: Counter
-    day: int
 
     def tick(self):
         """
         Spawn a new generation of fish!
         """
-        self.day += 1
         number_spawning = self.fish[0]
 
         # Decrease the age of the current population
-        new_counter: Counter[int] = Counter()
         for i in range(1, 9):
-            new_counter[i - 1] = self.fish[i]
+            self.fish[i - 1] = self.fish[i]
 
         # And add the new fish
-        new_counter[8] = number_spawning
+        self.fish[8] = number_spawning
 
         # Reset the old fish to 6, and add them to the current total
-        new_counter[6] += number_spawning
-        self.fish = new_counter
+        self.fish[6] += number_spawning
 
     def run(self, input_data: str, num_days: int) -> int:
-        self.day = 0
         self.fish = Counter(int(age) for age in input_data.split(","))
         for _ in range(num_days):
             self.tick()
