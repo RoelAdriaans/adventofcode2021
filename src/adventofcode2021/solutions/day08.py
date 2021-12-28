92828b32f053ebec0a65f4f1098e5de36091bd22
Roel Adriaans <roel@adriaans.org>
2021-12-28 21:49:12 +0100

    Day 8, part 2. Took some time..

--- src/adventofcode2021/solutions/day08.py
+++ src/adventofcode2021/solutions/day08.py
@@ -30,5 +30,106 @@ class Day08PartA(Day08, FileReaderSolution):
 
 
 class Day08PartB(Day08, FileReaderSolution):
+    def compute_sum(self, input_line) -> int:
+        return int("".join([str(x) for x in self.compute_digits(input_line)]))
+
+    def compute_digits(self, input_line) -> list[int]:
+        patterns, values = self.parse(input_line)
+        mapping = self.map(patterns)
+
+        total = []
+        for value in values:
+            value = "".join(sorted(value))
+            total.append(mapping[value])
+        return total
+
+    def map(self, patterns) -> dict[str, int]:
+        """
+        Try to reverse engineer the mapping on this line, and return the sum
+        of the digits
+        """
+        # Store every unique pattern we see, and remove them after we matched
+        pattern_set = set(["".join(sorted(v)) for v in patterns])
+
+        # Store the total mapping, this will contain letters -> Digit, eg:
+        # {"bcdf": 4, acdfg": 3}
+        mapping = {}
+
+        # First pick the easy values
+        # Map the length of the pattern to the digit, eg map "ba", length 2 to 1.
+        easy_map = {
+            2: 1,
+            4: 4,
+            3: 7,
+            7: 8,
+        }
+        for pattern in pattern_set.copy():
+            if digit := easy_map.get(len(pattern)):
+                mapping[pattern] = digit
+                pattern_set.discard(pattern)
+
+        # I know that 6 is everything EXCEPT one of 1's numbers.
+        # That can only be one:
+        # Mapping for 1 is:
+        mapping_one = self.find_mapping_for_digit(mapping, 1)
+        mapping_four = self.find_mapping_for_digit(mapping, 4)
+        for pattern in pattern_set.copy():
+            if len(pattern) == 6:
+                left_over = (set("abcdefg") - set(pattern)).pop()
+                if left_over in mapping_one:
+                    mapping[pattern] = 6
+                    pattern_set.discard(pattern)
+
+                # Mapping for 9 is length(6) with pattern with 4:
+                if all([letter in list(pattern) for letter in list(mapping_four)]):
+                    mapping[pattern] = 9
+                    pattern_set.discard(pattern)
+
+        # After this, the only item left with 6 characters is 0
+        for pattern in pattern_set.copy():
+            if len(pattern) == 6:
+                mapping[pattern] = 0
+                pattern_set.discard(pattern)
+
+        # Mapping for 3 is length(5) with pattern for one enabled
+        for pattern in pattern_set.copy():
+            if len(pattern) == 5:
+                # if mapping_one in pattern:
+                if all([letter in list(pattern) for letter in list(mapping_one)]):
+                    mapping[pattern] = 3
+                    pattern_set.discard(pattern)
+                    break
+
+        # Only 5 and 2 left.
+        # The digit that is OFF in the 6, must be ON in the 2.
+        mapping_six = self.find_mapping_for_digit(mapping, 6)
+        six_off = (set("abcdefg") - set(mapping_six)).pop()
+
+        for pattern in pattern_set.copy():
+            if six_off in pattern:
+                mapping[pattern] = 2
+                pattern_set.discard(pattern)
+                break
+
+        # Only 5 left
+        mapping[pattern_set.pop()] = 5
+        if len(pattern_set) != 0:
+            raise ValueError(f"Unmapped patterns left: {pattern_set}")
+
+        return mapping
+
+    @staticmethod
+    def find_mapping_for_digit(mapping: dict, needle: int) -> str:
+        """In the dict `mapping`, find the key for `needle`"""
+
+        for key, value in mapping.items():
+            if needle == value:
+                return key
+        else:
+            raise ValueError(f"Key {needle} not found")
+
     def solve(self, input_data: str) -> int:
-        raise NotImplementedError
+        """Where we really have to map, and stop counting :)"""
+
+        totals = [self.compute_sum(line) for line in input_data.splitlines() if line]
+        return sum(totals)
--- tests/solutions/test_day08_b.py
+++ tests/solutions/test_day08_b.py
@@ -1,11 +1,16 @@
-from adventofcode2021.solutions.day08 import Day08PartA
+from adventofcode2021.solutions.day08 import Day08PartB
 
 
-class TestDay08PartA:
+class TestDay08PartB:
     test_input = (
         "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab "
         "| cdfeb fcadb cdfeb cdbaf"
     )
+
+    def test_day08b_test_input(self):
+        solution = Day08PartB()
+        assert solution.solve(self.test_input) == 5353
+
     test_easy_digits = """
 be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
@@ -18,34 +23,37 @@ bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbg
 egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
 gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
 """
+    test_easy_digit_summed = [
+        8394,
+        9781,
+        1197,
+        9361,
+        4873,
+        8418,
+        4548,
+        1625,
+        8717,
+        4315,
+    ]
 
-    def test_day08a_simple(self):
-        solution = Day08PartA()
-        parsed = solution.parse(self.test_input)
-        assert parsed == (
-            [
-                "acedgfb",
-                "cdfbe",
-                "gcdfa",
-                "fbcad",
-                "dab",
-                "cefabd",
-                "cdfgeb",
-                "eafb",
-                "cagedb",
-                "ab",
-            ],
-            ["cdfeb", "fcadb", "cdfeb", "cdbaf"],
-        )
+    def test_day08b_forline(self):
+        test_easy_digits = self.test_easy_digits[1:].splitlines()
+        # Test that our lists are equal in length
+        assert len(test_easy_digits) == len(self.test_easy_digit_summed)
+        for line, excpected_sum in zip(test_easy_digits, self.test_easy_digit_summed):
+            expected_sum_list = list(map(int, str(excpected_sum)))
+            solution = Day08PartB()
+            calculated_sum = solution.compute_digits(line)
+            assert expected_sum_list == calculated_sum
 
-    def test_day08a_solve(self):
-        solution = Day08PartA()
+    def test_day08b_solve(self):
+        solution = Day08PartB()
         result = solution.solve(self.test_easy_digits)
 
-        assert result == 26
+        assert result == 61229
 
-    def test_day08a_data(self):
+    def test_day08b_data(self):
         """Result we got when we did the real solution"""
-        solution = Day08PartA()
+        solution = Day08PartB()
         res = solution("day_08/day08.txt")
-        assert res == 255
+        assert res == 982158
