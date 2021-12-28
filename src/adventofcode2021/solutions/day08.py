629fd6abd7e5b02bf705a2c20b0a0243fe97ee21
Roel Adriaans <roel.adriaans@asml.com>
2021-12-28 13:42:28 +0100

    We can also use a single list comprehension \o/

--- src/adventofcode2021/solutions/day08.py
+++ src/adventofcode2021/solutions/day08.py
@@ -20,13 +20,14 @@ class Day08PartA(Day08, FileReaderSolution):
         # Map a signal to the number of lines. Eg, number 7 uses 3 segments
         patterns = [self.parse(line) for line in input_data.splitlines() if line]
 
-        sum = 0
-        for pattern in patterns:
-            # Measure how many times we have a thing with 1, 4, 7, 8:
-            for value in pattern[1]:
-                if len(value) in (2, 4, 3, 7):
-                    sum += 1
-        return sum
+        accepted_values = (2, 4, 3, 7)
+
+        result = [
+            len(value) in accepted_values
+            for pattern in patterns
+            for value in pattern[1]
+        ]
+        return sum(result)
 
 
 class Day08PartB(Day08, FileReaderSolution):
